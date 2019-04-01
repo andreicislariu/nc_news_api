3f04c2829ca6a87cc41e874003f87dc8515a57ca
Andrei Florian <andreifc@Andreis-MacBook-Pro.local>
2019-04-01 14:55:16 +0100

    add tables for topics, users, articles, comments

---
+++ db/migrations/20190401124459_create_topics_table.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('topics', topics => {
+    topics.text('slug').primary();
+    topics.text('description');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('topics');
+};
---
+++ db/migrations/20190401125646_create_articles_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('articles', articles => {
+    articles.increments('article_id').primary();
+    articles.text('title');
+    articles.text('body');
+    articles.integer('votes').defaultTo(0);
+    articles.text('topic').references('topics.slug');
+    articles.text('author').references('users.username');
+    articles.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.droptTable('articles');
+};
