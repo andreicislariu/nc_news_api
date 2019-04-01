02cee3b1f300f470b78df634999a60dfc5c9aca9
Andrei Florian <andreifc@Andreis-MacBook-Pro.local>
2019-04-01 12:13:09 +0100

    add dev-data and test-data files

--- db/data/dev-data/index.js
+++ db/data/dev-data/index.js
@@ -1,1 +1,5 @@
 // require in and export out all dev data
+const articles = require('./articles');
+const comments = require('./comments');
+const topics = require('./topics');
+const users = require('./users');
--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,1 +1,5 @@
 // require in and export out all test data
+const articles = require('./articles');
+const comments = require('./comments');
+const topics = require('./topics');
+const users = require('./users');
